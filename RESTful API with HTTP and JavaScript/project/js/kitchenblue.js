03211ec8c6292158bbf31565aedf614d74181908
Ashiqul Hoque Chowdhury <ashiqulhoque45@gmail.com>
2020-05-24 20:55:08 +0600

    'Google IT Automation with Python'enrolled and 'Crash Course on Python' first week done 'Build Your First Android App (Project-Centered Course)'enrolled

--- RESTful API with HTTP and JavaScript/project/js/kitchenblue.js
+++ RESTful API with HTTP and JavaScript/project/js/kitchenblue.js
@@ -6,9 +6,11 @@
 
 // This function is called automatically after the web page is done loading
 
-$(document).ready(function() {
+$(document).ready(function () {
   // add an event listener (performSearch) to the form
-  $("#query-form").submit(function(event) { performSearch(event); });
+  $("#query-form").submit(function (event) {
+    performSearch(event);
+  });
 });
 
 // These website no longer work but are still returned by the recipe puppy
@@ -28,7 +30,9 @@ function isADefunctSite(sampleSite) {
 
   defunctDomains.forEach(
     function (item, index) {
-      if (sampleSite.includes(item)) { found = true; }
+      if (sampleSite.includes(item)) {
+        found = true;
+      }
     }
   );
 
@@ -45,21 +49,25 @@ function formatSearchResults(jsonResults) {
   var siteCount = 0;
 
   if (jsonObject.results.length == 0) { // Task 5: Part 1
-     setNotFoundMessages(); // Task 5: Part 1
-   } // Task 5: Part 1
-  // else { // Task 5: Part 1
+    setNotFoundMessages(); // Task 5: Part 1
+  } // Task 5: Part 1
+  else { // Task 5: Part 1
 
     $("#search-results-heading").text("Search Results");
     var formatedText = "";
 
     jsonObject.results.forEach(
-      function(item, index) {
+      function (item, index) {
 
-        // if (isADefunctSite(item.href)) { return; } // Task 5: Part 2
-        // siteCount++; // Task 5: Part 2
+        if (isADefunctSite(item.href)) {
+          return;
+        } // Task 5: Part 2
+        siteCount++; // Task 5: Part 2
 
         var thumbnail = item.thumbnail;
-        // if (thumbnail == "") { thumbnail = ""; }  // Task 5, Part 3, display images/generic_dish.jpg if thumbnail is empty
+        if (thumbnail == "") {
+          thumbnail = "images/generic_dish";
+        } // Task 5, Part 3, display images/generic_dish.jpg if thumbnail is empty
 
         const href = item.href;
 
@@ -69,14 +77,13 @@ function formatSearchResults(jsonResults) {
       }
     );
 
-    //if (siteCount > 0) { // Task 5: Part 2
+    if (siteCount > 0) { // Task 5: Part 2
       $("#results").html(formatedText);
-    // } // Task 5: Part 2
-    // else { // Task 5: Part 2
-      // setNotFoundMessages(); // Task 5: Part 2
-    // } // Task 5: Part 2
-  // } // Task 5: Part 1
-
+    } // Task 5: Part 2
+    else { // Task 5: Part 2
+      setNotFoundMessages(); // Task 5: Part 2
+    } // Task 5: Part 2
+  } // Task 5: Part 1
 }
 
 // This functions handles sending off the search request as well as
@@ -92,7 +99,7 @@ function performSearch(event) {
 
   // Abort any pending request
   if (request) {
-      request.abort();
+    request.abort();
   }
   // setup some local variables
   var $form = $(this);
@@ -106,30 +113,33 @@ function performSearch(event) {
   // Send the request
 
   request = $.ajax({
-      url: "https://cors-anywhere.herokuapp.com/" + "http://www.recipepuppy.com/api/",
-      type: "GET",
-      data: { i: $("#ingredients").val(), q:$"#contains".val() }
+    url: "https://cors-anywhere.herokuapp.com/" + "http://www.recipepuppy.com/api/",
+    type: "GET",
+    data: {
+      i: $("#ingredients").val(),
+      q: $("#contains").val()
+    }
   });
 
   // Callback handler for success
 
-  request.done(function (response, textStatus, jqXHR){
-      // formatSearchResults(response);  // Task 4 - uncomment
-      $("#results").html("<p>" + response + "</p>");  // Task 4 - comment out
+  request.done(function (response, textStatus, jqXHR) {
+    formatSearchResults(response); // Task 4 - uncomment
+    $("#results").html("<p>" + response + "</p>"); // Task 4 - comment out
   });
 
   // Callback handler for failure
 
-  request.fail(function (jqXHR, textStatus, errorThrown){
-      $("#search-results-heading").text("An error occurred. Please try again.");
-      $("#results").text("");
+  request.fail(function (jqXHR, textStatus, errorThrown) {
+    $("#search-results-heading").text("An error occurred. Please try again.");
+    $("#results").text("");
   });
 
   // Callback handler that will be called in any case
 
   request.always(function () {
-      // Reenable the inputs
-      setFormDisabledProps(false);
+    // Reenable the inputs
+    setFormDisabledProps(false);
   });
 
 }
@@ -156,10 +166,10 @@ function sanitizeInputs() {
 // This function disables the text fields and the two buttons
 
 function setFormDisabledProps(statusToSet) {
-    document.getElementById("ingredients").disabled = statusToSet;
-    document.getElementById("contains").disabled = statusToSet;
-    document.getElementById("resetButton").disabled = statusToSet;
-    document.getElementById("searchButton").disabled = statusToSet;
+  document.getElementById("ingredients").disabled = statusToSet;
+  document.getElementById("contains").disabled = statusToSet;
+  document.getElementById("resetButton").disabled = statusToSet;
+  document.getElementById("searchButton").disabled = statusToSet;
 }
 
 // This function sets the result heading to "no recipes found" and clear the
